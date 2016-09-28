eb17a8a051e373af54fa9a0027bdf9fe6e66d1c9
Harryson Pointdujour <Harryson Pointdujour>
2016-09-28 19:34:03 -0400

    initial seconds functionality

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,37 @@
 (function() {
 //initialize variables
   var startButton = $('#start');
+  var seconds = $('#seconds');
 
 //main functionality
   startButton.on('click', countdown);
 
 //function definitions
-function countdown (){
-  alert("It's the final countdown!!!");
+function countdown(){
+  var secondsText = seconds.text();
+  var secondsTextAsNumber = parseInt(secondsText);
+  if(secondsTextAsNumber === 0){
+    seconds.text("59");
+    // then change seconds text to 59 (use triple ===)
+  }else{
+    var decreasedSecondsAsNumberByOne = secondsTextAsNumber -1;
+    var padSecondsTextAsNumber = pad(decreasedSecondsAsNumberByOne);
+    seconds.text(padSecondsTextAsNumber); //this writes inside of the html
 
+  }
+
+  // var secondsValue = parseInt(seconds.text());
+  // seconds.text(pad(secondsValue-1));
+}
+
+function pad(num){
+  if(num < 10){
+    // spit out number with a leading 0
+    return "0" + num;
+  } else{
+    // spit out the original num
+    return num;
+  }
 }
 
 
