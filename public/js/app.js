033a9c95c5dbc17f9181dfb30a931cb673905fad
Harryson Pointdujour <Harryson Pointdujour>
2016-09-28 20:21:18 -0400

    added the ability for the minutes to cycle based on seconds and stopped when minutes and seconds are both equal t o zero

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,7 @@
 //initialize variables
   var startButton = $('#start');
   var seconds = $('#seconds');
+  var minutes = $('#minutes');
 
 //main functionality
   startButton.on('click', countdown);
@@ -10,8 +11,22 @@
 function countdown(){
   var secondsText = seconds.text();
   var secondsTextAsNumber = parseInt(secondsText);
+  var minutesText = minutes.text();
+  var minutesTextAsNumber = parseInt(minutesText);
+
+if(minutesTextAsNumber ===0 && secondsTextAsNumber ===0 ){
+  // stop!
+  return; //temporary
+}
   if(secondsTextAsNumber === 0){
+    if (minutesTextAsNumber === 0){
+
+    }else {
+
+    }
+
     seconds.text("59");
+    minutes.text(padMinutesTextAsNumber);
     // then change seconds text to 59 (use triple ===)
   }else{
     var decreasedSecondsAsNumberByOne = secondsTextAsNumber -1;
