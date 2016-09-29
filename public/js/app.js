71b657f497b829c9322f011fe4e53e8838c68c7f
Harryson Pointdujour <Harryson Pointdujour>
2016-09-28 20:54:59 -0400

    fixed issue with clock

--- public/js/app.js
+++ public/js/app.js
@@ -1,53 +1,55 @@
 (function() {
-//initialize variables
+  //initialize variables
   var startButton = $('#start');
   var seconds = $('#seconds');
   var minutes = $('#minutes');
+  var timerInterval;
+  // main functionality
+  startButton.on('click', startTimer);
 
-//main functionality
-  startButton.on('click', countdown);
-
-//function definitions
-function countdown(){
-  var secondsText = seconds.text();
-  var secondsTextAsNumber = parseInt(secondsText);
-  var minutesText = minutes.text();
-  var minutesTextAsNumber = parseInt(minutesText);
-
-if(minutesTextAsNumber ===0 && secondsTextAsNumber ===0 ){
-  // stop!
-  return; //temporary
-}
-  if(secondsTextAsNumber === 0){
-    if (minutesTextAsNumber === 0){
-
-    }else {
-
+  //function definitions
+  function startTimer(){
+    if(!timerInterval){
+        timerInterval = setInterval(countdown, 1000);
+    }
+  }
+  function countdown(){
+    var secondsText = seconds.text();
+    var secondsTextAsNumber = parseInt(secondsText);
+    var minutesText = minutes.text();
+    var minutesTextAsNumber = parseInt(minutesText);
+    // console.log(typeof secondsText);
+    // console.log(typeof secondsTextAsNumber);
+    if(minutesTextAsNumber === 0 && secondsTextAsNumber === 0){
+      //stop!
+      return; //temporary
+    }
+    if(secondsTextAsNumber === 0) {
+      if(minutesTextAsNumber !== 0){
+        var decreasedMinutesAsNumberByOne = minutesTextAsNumber - 1;
+        var padMinutesTextAsNumber = pad(decreasedMinutesAsNumberByOne);
+        minutes.text(padMinutesTextAsNumber);
+      }
+      seconds.text("59");
+      //then change seconds text to 59
+    } else {
+      var decreasedSecondsAsNumberByOne = secondsTextAsNumber - 1;
+      var padSecondsTextAsNumber = pad(decreasedSecondsAsNumberByOne);
+      seconds.text(padSecondsTextAsNumber); //this writes inside of the html
     }
 
-    seconds.text("59");
-    minutes.text(padMinutesTextAsNumber);
-    // then change seconds text to 59 (use triple ===)
-  }else{
-    var decreasedSecondsAsNumberByOne = secondsTextAsNumber -1;
-    var padSecondsTextAsNumber = pad(decreasedSecondsAsNumberByOne);
-    seconds.text(padSecondsTextAsNumber); //this writes inside of the html
-
+    // var secondsValue = parseInt(seconds.text());
+    //
+    // seconds.text(pad(secondsValue - 1));
   }
 
-  // var secondsValue = parseInt(seconds.text());
-  // seconds.text(pad(secondsValue-1));
-}
-
-function pad(num){
-  if(num < 10){
-    // spit out number with a leading 0
-    return "0" + num;
-  } else{
-    // spit out the original num
-    return num;
+  function pad(num){
+    if(num < 10){
+      //spit out the number with a leading zero
+      return "0" + num;
+    } else {
+      // spit out the original number
+      return num;
+    }
   }
-}
-
-
 }());
